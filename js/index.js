cb60394d88c69850c64fa4a47e58a9f84fb3ae2b
akib <akibns57@gmail.com>
2026-03-05 01:32:27 +0600

    feat: add lesson fetch, level-based vocabulary load and empty state handling

---
+++ js/index.js
@@ -0,0 +1,67 @@
+const loadLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res) => res.json())
+    .then((json) => displayLesson(json.data));
+}
+const loadLevelWord=(id)=>{
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then(res =>res.json())
+    .then(data => displayLevelWord((data.data)));
+}
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+
+    if(words.length === 0){
+        wordContainer.innerHTML = `
+        <div class="text-center bg-sky-100 col-span-full rounded-xl py-10 space-y-6 font-bangla">
+        <img class= "mx-auto" src="./assets/alert-error.png" alt="">
+    <p class="text-xl font-medium text-gray-400">
+        এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
+    </p>
+    <h2 class="font-bold text-4xl">
+        নেক্সট Lesson এ যান
+    </h2>
+</div>
+`;
+    }
+
+    words.forEach((word) => {
+        // console.log(word);
+        const card = document.createElement("div");
+        card.innerHTML = `
+            <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-6">
+        <h2 class="font-bold text-2xl"> ${word.word ? word.word : "শব্দ পাওয়া যায়নি"} </h2>
+        <p class="font-semibold">Meaning /Pronunciation</p>
+        <div class="font-bangla text-2xl font-medium">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি "}"</div>
+        <div class="flex justify-between items-center">
+            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+            <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+    </div>
+        `;
+        wordContainer.append(card);
+    })
+}
+
+
+const displayLesson = (lessons) => {
+    // 1: get the container & empty
+    const levelContainer = document.getElementById("level-container")
+    levelContainer.innerHTML = ""
+    // 2: get into every lessons
+    for(let lesson of lessons){
+    // 3: create Element
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+    <button onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+    <i class="fa-solid fa-circle-question"></i>Lesson - ${lesson.level_no}
+    </button>`;
+    
+    // 4: append into container
+    levelContainer.append(btnDiv);
+    }
+}
+loadLessons();
